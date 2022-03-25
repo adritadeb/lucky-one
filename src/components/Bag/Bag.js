0947ee6a759243445c9ba5a9c761f0fe80bda7b6
adritadeb <adritad2019@gmail.com>
2022-03-26 01:14:41 +0600

    add-cart-icon

--- src/components/Bag/Bag.js
+++ src/components/Bag/Bag.js
@@ -1,14 +1,17 @@
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Bag.css';
 
-const Bag = (props) => {
-    const { img, name, price } = props.bag;
+const Bag = ({ bag }) => {
+    const { img, name, price } = bag;
     return (
         <div className='bag-info'>
             <img src={img} alt="" />
             <h2>{name}</h2>
             <h3>Price: {price}</h3>
-            <button className='add-btn'>Add to Cart</button>
+            <button className='add-btn'>Add to Cart
+                <FontAwesomeIcon className='cart-icon' icon={faCartPlus}></FontAwesomeIcon></button>
         </div>
     );
 };
